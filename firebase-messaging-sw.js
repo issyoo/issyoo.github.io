3a181b6ee4ceeb53a64eec1405a67f001dc5d779
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2022-12-15 07:34:16 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -46,7 +46,7 @@
  messaging.onBackgroundMessage(function(payload) { 
    console.log( "[firebase-messaging-sw.js] Received background message ",  payload );
   const notificationTitle = payload.notification.title;
-  const notificationOptions = { body: payload.notification.body, icon: 'https://issyoo.github.io/IMG_20221121_171539.jpg', image: payload.notification.image };
+  const notificationOptions = { body: payload.notification.body, icon: payload.notification.icon, image: payload.notification.image };
   
    self.registration.showNotification(notificationTitle,  notificationOptions); 
  });
