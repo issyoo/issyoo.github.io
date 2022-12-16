148287c85303cda9fd95406817b0bfb11ec04fe9
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2022-12-16 11:05:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -168,3 +168,36 @@ function getParameterByName(name, url = window.location.href) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
     newData();
+function createRipple() {
+
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+
+    ripple.remove();
+
+  }
+
+  button.appendChild(circle);
+}
+
+const divs = document.getElementsByClassName("div");
+for (const div of divs) {
+  div.addEventListener("touchstart", createRipple);
+}
