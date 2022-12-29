68c20736383bb751fb8495480c3046687df446a1
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2022-12-29 09:18:51 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -44,7 +44,7 @@
  // For more info see:  
  // https://firebase.google.com/docs/cloud-messaging/concept-options 
  messaging.onBackgroundMessage(function(payload) { 
-   console.log( "[firebase-messaging-sw.js] Received background message ",  payload );
+   console.log( "[firebase-messaging-sw.js] Received background message "; //,  payload );
   const notificationTitle = payload.notification.title;
   const notificationOptions = { body: payload.notification.body, icon: payload.notification.icon, image: payload.notification.image };
   
