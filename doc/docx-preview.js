b72c5335245c2220b7aae729275ad25fb989787e
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-02-18 09:11:00 +0530

    Update docx-preview.js

--- doc/docx-preview.js
+++ doc/docx-preview.js
@@ -2804,14 +2804,14 @@ class HtmlRenderer {
     renderDefaultStyle() {
         var c = this.className;
         var styleText = `
-.${c}-wrapper { background: gray; padding: 30px; padding-bottom: 0px; display: flex; flex-flow: column; align-items: center; } 
+.${c}-wrapper { background: gray; padding: 30px; padding-bottom: 0px; display: ''; flex-flow: column; align-items: center; } 
 .${c}-wrapper>section.${c} { background: white; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); margin-bottom: 30px; }
 .${c} { color: black; }
 section.${c} { box-sizing: border-box; display: flex; flex-flow: column nowrap; position: relative; overflow: hidden; }
 section.${c}>article { margin-bottom: auto; }
 .${c} table { border-collapse: collapse; }
 .${c} table td, .${c} table th { vertical-align: top; }
-.${c} p { margin: 0pt; width: 100vw; }
+.${c} p { margin: 0pt; min-height: 1em; }
 .${c} span { white-space: pre-wrap; overflow-wrap: break-word; }
 .${c} a { color: inherit; text-decoration: inherit; }
 `;
