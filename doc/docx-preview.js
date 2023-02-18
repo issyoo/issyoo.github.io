d122e2e45d48a02ed20f59976c501c42723b042c
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-02-18 09:15:04 +0530

    Update docx-preview.js

--- doc/docx-preview.js
+++ doc/docx-preview.js
@@ -2804,7 +2804,7 @@ class HtmlRenderer {
     renderDefaultStyle() {
         var c = this.className;
         var styleText = `
-.${c}-wrapper { background: gray; padding: 30px; padding-bottom: 0px; display: ''; flex-flow: column; align-items: center; } 
+.${c}-wrapper { background: gray; padding: 30px; padding-bottom: 0px; display: flex; flex-flow: column; align-items: center; overflow: scroll;} 
 .${c}-wrapper>section.${c} { background: white; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); margin-bottom: 30px; }
 .${c} { color: black; }
 section.${c} { box-sizing: border-box; display: flex; flex-flow: column nowrap; position: relative; overflow: hidden; }
