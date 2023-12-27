17cab5bfacdfb7401cd85e5d8628e081a787d60d
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2023-12-27 16:38:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,18 +15,16 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-storage.js";
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    //...
-};
+    apiKey: "AIzaSyD_yGXpo3fn4mZ9aGidodxeGLevGy-C5xU",
+    authDomain: "is-issyoo.firebaseapp.com",
+    projectId: "is-issyoo",
+    storageBucket: "is-issyoo.appspot.com",
+    messagingSenderId: "43828649134",
+    appId: "1:43828649134:web:650ddf1bf69942886fe13f",
+    measurementId: "G-4Y2YEDJ2M7"
+  };
 
-const app = initializeApp({
-    apiKey: "AIzaSyAN-X9u7n7QnM8RLrVV4YnXq2MmVz6WNfw",
-    authDomain: "em-issyoo.firebaseapp.com",
-    projectId: "em-issyoo",
-    storageBucket: "em-issyoo.appspot.com",
-    messagingSenderId: "33480809995",
-    appId: "1:33480809995:web:74df1e056f7638d0b5310e",
-    measurementId: "G-C64NWHRT0Y"
-});
+const app = initializeApp(app);
 
 const analytics = getAnalytics(app);
 
